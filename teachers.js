b317377f52006d6cebe13695c23e42b7dbbc1820
Timo Triisa <timotriisa@gmail.com>
2024-04-10 11:39:02 +0300

    GM download URL

--- teachers.js
+++ teachers.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         Tahvel Täiendused Õpetajale
+// @name         Täiendatud Tahvel Õpetajale
 // @namespace    https://tahvel.edu.ee/
-// @version      1.0
+// @version      1.0.0
 // @description  Tahvlile mõned UI täiendused, mis parandavad tundide sisestamist ja hindamist.
 // @author       Timo Triisa
 // @match        https://tahvel.edu.ee/*
+// @downloadURL  https://raw.githubusercontent.com/Tallinna-Polutehnikum/tahvel-userscripts/main/teachers.js
 // @grant GM_log
 // ==/UserScript==
 
